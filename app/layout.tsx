dd1078552744bbd163336a7543b223804c3a8162
gauravsahdz <sahgaurav33@gmail.com>
2023-12-31 22:14:27 +0545

    meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
@@ -9,6 +8,15 @@ import { LoaderProvider } from "@/context/LoaderProvider";
 import React from "react";
 import { AlertProvider } from "@/context/AlertProvider";
 import Head from "next/head";
+
+export const metadata = {
+  metadataBase : new URL("https://online-store-gauravsahdz.vercel.app"),
+  title: {
+    default : "OnlineStore",
+    template : `%s | OnlineStore`,
+  },
+  description: "Shop Clothing, personalizations, regular use, ornaments, ect....",
+}
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import HomePage from "@/app/(pages)/[homepage]/page";
 
-export const metadata = {
-  title: "Online Store",
-  description: "Shop Clothing, personalizations, regular use, ornaments, ect....",
-}
+
 
 export default function Home() {
   return (
